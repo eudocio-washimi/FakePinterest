25ad6b504917acc93b2c106143b4caebf6fae9f1
eudocio-washimi <eudocio.washimi@fatec.sp.gov.br>
2025-09-01 22:59:06 -0300

    Criação da funcionalidade de postar fotos
    Agora é possível adicionar fotos por meio dos perfis

--- routes.py
+++ routes.py
@@ -2,8 +2,10 @@
 from flask import render_template, url_for, redirect
 from fakepinterest import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
-from fakepinterest.forms import FormLogin, FormCadastro
+from fakepinterest.forms import FormLogin, FormCadastro, FormFoto
 from fakepinterest.models import Usuario, Foto
+import os
+from werkzeug.utils import secure_filename
 
 
 @app.route("/", methods=["GET","POST"]) #Caminho (rota) do link para o meu site
@@ -31,16 +33,28 @@ def cadastro():
     return render_template("cadastro.html", form=form_cadastro)
 
 
-@app.route("/perfil/<id_usuario>")
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
     if int(id_usuario) == int(current_user.id):
         # o usuário está visualizando o próprio perfil
-        return render_template("perfil.html", usuario=current_user)
+        form_foto = FormFoto()
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            nome_seguro = secure_filename(arquivo.filename)
+            # salvar o arquivo na pasta fotos_post
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+                              app.config["UPLOAD FOLDER"], nome_seguro)
+            arquivo.save(caminho)
+            #registrar arquivo no banco de dados
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+        return render_template("perfil.html", usuario=current_user, form=form_foto)
     else:
         # está visualizando o perfil de um terceiro
         usuario = Usuario.query.get(int(id_usuario))
-        return render_template("perfil.html", usuario=usuario)
+        return render_template("perfil.html", usuario=usuario, form=None)
 
 
 @app.route("/logout")
